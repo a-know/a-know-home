1106ab570393309aee836ca6b3225862aab88a77
a-know <a.know.3373@gmail.com>
2018-02-10 10:47:50 +0900

    Delete all

--- war/assets/js/original_contents.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  var redirect_url = "http://home.a-know.me";
-  if (document.referrer) {
-    var referrer = "referrer=" + encodeURIComponent(document.referrer);
-    redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
-  }
-  location.href = redirect_url;
-});
